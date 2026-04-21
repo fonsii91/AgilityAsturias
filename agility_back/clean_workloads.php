838521f45885dbc8def432142dc1bf789a85e863
fonsi <alfonsogar13@gmail.com>
2026-04-21 14:46:45 +0200

    Cleanup script removal

--- agility_back/clean_workloads.php
+++
@@ -1,5 +0,0 @@
-<?php
-\App\Models\DogWorkload::where('source_type', 'auto_competition')->whereNotIn('source_id', function($q) {
-    $q->select('id')->from('competitions')->where('tipo', 'competicion');
-})->delete();
-echo "Cleanup done\n";
