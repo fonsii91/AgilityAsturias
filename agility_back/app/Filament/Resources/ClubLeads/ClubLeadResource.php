814828c659041710dfd3f579e8432cd2879437af
fonsi <alfonsogar13@gmail.com>
2026-05-09 05:58:13 +0200

    Fix PHP type mismatch for navigationIcon

--- agility_back/app/Filament/Resources/ClubLeads/ClubLeadResource.php
+++ agility_back/app/Filament/Resources/ClubLeads/ClubLeadResource.php
@@ -20,7 +20,7 @@ class ClubLeadResource extends Resource
 {
     protected static ?string $model = ClubLead::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-rectangle-stack';
 
     protected static ?string $recordTitleAttribute = 'name';
 
