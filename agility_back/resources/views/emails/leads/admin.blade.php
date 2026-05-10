879b26e4dffce36a327f181aab62109c616cf8fe
fonsi <alfonsogar13@gmail.com>
2026-05-10 23:57:46 +0200

    mails

--- agility_back/resources/views/emails/leads/admin.blade.php
+++ agility_back/resources/views/emails/leads/admin.blade.php
@@ -1,21 +1,21 @@
 <x-mail::message>
-# ¡Nuevo Club Lead!
+    # ¡Nuevo Club Lead!
 
-Se acaba de recibir una nueva solicitud de alta de club a través de la web principal.
+    Se acaba de recibir una nueva solicitud de alta de club a través de la web principal.
 
-**Datos del Club:**
-- **Nombre:** {{ $lead->name }}
-- **Email:** {{ $lead->email }}
-- **Teléfono:** {{ $lead->phone }}
-- **Subdominio deseado:** {{ $lead->slug }}.clubagility.com
-- **Plan seleccionado:** {{ $lead->plan_selected }}
+    **Datos del Club:**
+    - **Nombre:** {{ $lead->name }}
+    - **Email:** {{ $lead->email }}
+    - **Teléfono:** {{ $lead->phone }}
+    - **Subdominio deseado:** {{ $lead->slug }}.clubagility.com
+    - **Plan seleccionado:** {{ $lead->plan_selected }}
 
-Por favor, accede al panel de SuperAdmin para procesar esta solicitud.
+    Por favor, accede al panel de SuperAdmin para procesar esta solicitud.
 
-<x-mail::button :url="'https://admin.clubagility.com/club-leads'">
-Ver Leads en Panel Admin
-</x-mail::button>
+    <x-mail::button :url="'https://admin.clubagility.com/club-leads'">
+        Ver Leads en Panel Admin
+    </x-mail::button>
 
-Saludos,<br>
-El sistema
+    Saludos,<br>
+    El sistema
 </x-mail::message>
