793a49830533847e48c8e6cd11a845257e816730
fonsi <alfonsogar13@gmail.com>
2026-06-02 02:10:02 +0200

    Clean up temporary database update script

--- agility_back/update_prod.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$updated = \App\Models\Video::withoutGlobalScopes()->whereIn('id', [326, 327])->update(['club_id' => 2]);
-echo "Updated {$updated} videos!\n";
